872dc39bcf0c1c568517a6e52629716bdd42e11b
Dan Stuart <drestuart@gmail.com>
2019-05-16 23:28:48 -0400

    Actually hooked up api to db connector

--- api/swagger_server/controllers/geo_ip_controller.py
+++ api/swagger_server/controllers/geo_ip_controller.py
@@ -4,15 +4,19 @@ import six
 from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
 from swagger_server import util
 
+from ..postgres_connector import PostgresConnector
 
-def get_coords(ip):  # noqa: E501
+
+def get_coords(ip):
     """
-
-     # noqa: E501
-
     :param ip: IPv4 or IPv6 address
     :type ip: str
 
     :rtype: InlineResponse200
     """
-    return 'do some magic!'
+    connector = PostgresConnector()
+    connector.connect()
+    result = connector.query_ipv4(ip)
+    connector.disconnect()
+
+    return result
